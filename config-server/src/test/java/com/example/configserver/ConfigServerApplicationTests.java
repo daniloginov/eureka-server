5d0ae0b45d4b2e14a3cdebfdd56cc79e4e75ba36
Daniil Loginov <daniloginov@gmail.com>
2025-01-22 21:07:31 +0500

    Добавил BookService и Client Service

--- config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/example/configserver/ConfigServerApplicationTests.java
@@ -1,8 +1,6 @@
 package com.example.configserver;
 
-import org.junit.Test;
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
--- eureka-client/src/main/java/eurekaClient/EurekaClientApplication.java
+++ eureka-client/src/main/java/eurekaClient/EurekaClientApplication.java
@@ -9,9 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(EurekaClientApplication.class, args);
-
     }
 
 }
